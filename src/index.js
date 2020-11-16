2aa3139deb4cef2fe62c4900db8ba374df7f924a
rangheet <heetdave@outlook.com>
2020-11-16 11:23:25 -0700

    Added data files and basic geoContains

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
+
+var geoData;
+
 document.addEventListener('DOMContentLoaded', function() {  
 
-    console.log("Hello world")
-        
+    Promise.all([d3.json ("data/StHimark.json")]).then(data=>{
+            geoData = data[0];
+            draw();
+    });
+
 });
+
+function draw()
+{
+
+    geoData.geometries.forEach((element, idx) => {
+        if(d3.geoContains(element, [-13336289,17841]))
+        {
+            console.log(element, idx);
+        }
+    }); 
+}
+
