1f93a439b3d0070b0efd8472934152dd2e1218b3
Steve Jadav <ssjadav@asu.edu>
2020-11-27 11:11:35 +0530

    Latest

--- src/index.js
+++ src/index.js
@@ -1,16 +1,44 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+   const FACTORY_GLYPH = "M456.723,121,328.193,248H312V121H291.3L166.084,248H152V32H32V480H480V121ZM172,432H132V392h40Zm0-80H132V312h40Zm80,80H212V392h40Zm0-80H212V312h40Zm80,80H292V392h40Zm0-80H292V312h40Zm80,80H372V392h40Zm0-80H372V312h40Z";
+   const HOSPITAL_GLYPH = "M352,104V208H160V104H88V448H238V376h38v72H424V104ZM197,394H157V354h40Zm0-92H157V262h40Zm80,0H237V262h40Zm80,92H317V354h40Zm0-92H317V262h40ZM352,104V208H160V104H88V448H238V376h38v72H424V104ZM197,394H157V354h40Zm0-92H157V262h40Zm80,0H237V262h40Zm80,92H317V354h40Zm0-92H317V262h40Z";
+   const MOBILE_SENSOR_IDX = [ 15, 22, 40,  1, 27, 30,  8, 41,  9, 37, 26, 16, 49, 13,  2, 31, 44,
+                     6, 43, 14, 11, 23, 32,  3,  5, 35, 24,  4, 34, 45, 47, 39, 19, 29,
+                     38, 12, 33, 17, 46, 10,  7, 18, 20, 50, 28, 48, 36, 25, 42, 21 ];
+
+    // Populate the mobile sensor dropdown
+    var selectpicker = d3.select(".navbar")
+                     .select(".selectpicker");
+
+
+    MOBILE_SENSOR_IDX.forEach(id => {
+      selectpicker.append("option")
+                  .text("Mobile Sensor " + id)
+                  .attr("value", id);
+    });
+
+
+    // Define the div for the tooltip
+    var toolTipDiv;
+    toolTipDiv = d3.select("body")
+                 .append("div")
+                 .attr("class", "tooltip")
+                 .style("opacity", 0);
+
+    var parseTime = d3.timeParse("%Y-%m-%d %H:%M:%S");
+    var alwaysSafePlantLocation = [ -119.784825, 0.162679 ];
+
     var lineSvg = d3.select(".staticSensorLineChart")
-        .attr("width", 1264)
-        .attr("height", 750);
+                    .attr("width", 1120)
+                    .attr("height", 850);
 
     var map = d3.select(".map")
-        .attr("width", 1264)
-        .attr("height", 550);
+                .attr("width", 600)
+                .attr("height", 550);
 
-    var heat = d3.select(".heat")
-        .attr("width", 1264)
-        .attr("height", 1100);
+    var barChart = d3.select(".barChart")
+                .attr("width", 610)
+                .attr("height", 550);
 
 
     var sensorProximitySVG = d3.select(".sensorProximity")
@@ -20,174 +48,42 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Setting projection parameters
     var mapProjection = d3.geoMercator()
-        .scale(135000)
-        .center([-119.88075, 0.125])
-        .translate([500, 250]);
+                          .scale(120000)
+                          .center([ -119.88075, 0.125 ])
+                          .translate([ 220, 250 ]);
 
-
-    // Create geoPath function that uses built-in D3 functionality
-    // to turn geographical coordinates into screen coordinates
     var geoPath = d3.geoPath().projection(mapProjection);
 
+
     d3.queue()
-        .defer(d3.json, "data/StHimark.json")
-        .defer(d3.csv, "data/StaticSensorLocations.csv")
-        .defer(d3.csv, "data/StaticSensorReadingsAggregate.csv")
-        .await(drawMap);
+      .defer(d3.json, "data/StHimark.json")
+      .defer(d3.csv, "data/StaticSensorLocations.csv")
+      .defer(d3.csv, "data/StaticSensorReadingsAggregate.csv")
+      .defer(d3.csv, "data/MobileSensorReadingsAggregate.csv")
+      .defer(d3.csv, "data/HospitalLocations.csv")
+      .await(drawMap);
 
 
-    function drawMap(error, geoData, staticSensorLocations, staticSensorReadings) {
+    function drawMap(error, geoData, staticSensorLocations, staticSensorReadings, mobileSensorReadings, hospitalLocations) {
 
-        if (error) console.log(error);
+      if (error) console.log(error);
 
-        /* Hashmap for associating area ID with sensor-id { areaID: sensorID }
-        var hashmap = {};
-        staticSensorLocations.forEach(function(d) {
-          let point = [parseFloat(d.Long), parseFloat(d.Lat)];
-          for (var i = 0; i < geoData.features.length; i++) {
-            if (d3.geoContains(geoData.features[i], point))
-              hashmap[geoData.features[i].properties["Id"]] = d["Sensor-id"];
+      // Hashmap for associating area ID with sensor-id { areaID: sensorID }
+      var hashmap = new Map();
+      staticSensorLocations.forEach(function(d) {
+        let point = [ parseFloat(d.Long), parseFloat(d.Lat) ];
+        for (var i = 0; i < geoData.features.length; i++) {
+
+          let locationID = geoData.features[i].properties["Id"];
+
+          if (d3.geoContains(geoData.features[i], point)) {
+            if (hashmap.has(locationID)) {
+              hashmap.get(locationID).push( d["Sensor-id"] );
+            }
+            else {
+              hashmap.set(locationID, [ d["Sensor-id"] ]);
+            }
           }
-        });
-        */
-
-        /* Radiatian Measurements every 6 minutes grouped-by area ID
-        var raditionMeasurements = {};
-        geoData.features.forEach(function(d) {
-          // For each area, first find the sensor present in that area, and then find its corresponding radiation measurements
-          let regionID = d.properties["Id"];
-          let sensorID = hashmap[regionID];
-          raditionMeasurements[sensorID] = [];
-
-          // filter staticSensorAggregateData for this particular sensorID
-          let curr = staticSensorReadings.filter(function(x) {
-            return x["Sensor-id"] == sensorID;
-          });
-
-          curr.forEach(function(x) {
-            raditionMeasurements[sensorID].push(parseFloat(x.Value));
-          });
-
-        }); */
-
-        // Radiation Measurements for each static sensor
-        radiationMeasurements = {};
-        staticSensorLocations.forEach(function(d) {
-            let sensorID = d["Sensor-id"];
-            radiationMeasurements[sensorID] = [];
-            let curr = staticSensorReadings.filter(function(x) {
-                return x["Sensor-id"] == sensorID;
-            });
-
-            curr.forEach(function(x) {
-                radiationMeasurements[sensorID].push(parseFloat(x.Value));
-            });
-        });
-
-
-        geo_map = map.append("g")
-            .attr("class", "st-himark-map")
-            .selectAll("path")
-            .data(geoData.features)
-            .enter()
-            .append("path")
-            .attr("d", geoPath)
-            .style("fill", "black")
-            .style("stroke", "gray");
-
-
-        // Neighborhood names at the centroid of each polygon
-        map.append("g")
-            .attr("class", "neighborhood-names")
-            .selectAll("text")
-            .data(geoData.features)
-            .enter()
-            .append("svg:text")
-            .text(function(d) {
-                return d.properties.Name;
-            })
-            .attr("x", function(d) {
-                let temp = {};
-                temp.geometry = d.geometry;
-                temp.geometry.type = "MultiPolygon";
-                temp.geometry.coordinates = [d.geometry.coordinates[0]];
-                return geoPath.centroid(temp.geometry)[0];
-            })
-            .attr("y", function(d) {
-                let temp = {};
-                temp.geometry = d.geometry;
-                temp.geometry.type = "MultiPolygon";
-                temp.geometry.coordinates = [d.geometry.coordinates[0]];
-                return geoPath.centroid(temp.geometry)[1];
-            })
-            .attr("text-anchor", "middle")
-            .attr("fill", "white")
-            .style("font-size", "9px");
-
-
-        map.append("g")
-            .attr("class", "static-sensors")
-            .selectAll("rect")
-            .data(staticSensorLocations)
-            .enter()
-            .append("circle")
-            .attr("id", function(d) { return "static-sensor-" + d["Sensor-id"]; })
-            .attr("cx", function(d) {
-                let coordinates = [parseFloat(d.Long), parseFloat(d.Lat)];
-                return mapProjection(coordinates)[0];
-            })
-            .attr("cy", function(d) {
-                let coordinates = [parseFloat(d.Long), parseFloat(d.Lat)];
-                return mapProjection(coordinates)[1];
-            })
-            .attr("r", 2)
-            .style("fill", "#33e613")
-            .style("opacity", 1)
-            .style("stroke", "#33e613");
-
-        var circles = d3.selectAll("circle");
-        pulse(circles);
-
-        // The pulsating effect shows spikes in radiation measurements. When the radius of the
-        // circle increases, it is an indication of radiation detection.
-
-        function pulse(circle) {
-
-            let i = 0;
-            (function repeat() {
-
-                circle
-                    .transition()
-                    .duration(100)
-                    .attr("stroke-width", 0)
-                    .attr('stroke-opacity', 0)
-                    .transition()
-                    .duration(100)
-                    .attr("stroke-width", 0)
-                    .attr('stroke-opacity', 0.5)
-                    .style("fill", function(d) {
-                        if (radiationMeasurements[d["Sensor-id"]][i] > 15) return "red";
-                        else return "#33e613";
-                    })
-                    .style("stroke", function(d) {
-                        if (radiationMeasurements[d["Sensor-id"]][i] > 15) return "red";
-                        else return "#33e613";
-                    })
-                    .attr("r", function(d) {
-                        if (radiationMeasurements[d["Sensor-id"]][i] > 15) return 10;
-                        else return 2;
-                    })
-                    .transition()
-                    .duration(1000)
-                    .attr("stroke-width", function(d) { return radiationMeasurements[d["Sensor-id"]][i] + 70; })
-                    .attr('stroke-opacity', 0)
-                    .ease(d3.easeSin)
-                    .on("end", repeat);
-
-                if (i == 1200) i = -1;
-                i += 1;
-
-            })();
         }
       });
 
